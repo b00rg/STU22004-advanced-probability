116b72de2e230876aa54183f109ffc3678340b90
Emma Burgess <66336643+b00rg@users.noreply.github.com>
2024-11-12 09:45:22 +0000

    Update Question1.py
    incorporating steps for continuous measurement

--- Question1.py
+++ Question1.py
@@ -2,19 +2,22 @@ from math import sqrt
 import numpy as np
 import matplotlib.pyplot as plt
 
-temperature = 0.0
 number_of_steps = 0.0
 sigma = 0.2 #volatility
-delta_ts = [0.01, 0.001, 0.0001, 0.00001, 0.000001] # time increments 
+delta_ts = [0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.000001] # time increments 
 num_simulations = 100
-above_zero_temp = 0 
+time = 1 
 
-for t in range(1, num_simulations):
-    Z = np.random.standard_normal() # standard normal random variable 
-    temperature_change = sigma*sqrt(delta_ts[0])*Z  # this is based off a stochastic process. change delta_ts[x] for different time increments
-    temperature += temperature_change # over time, temp changes with respect to temperature_change
-    if temperature > 0.0:
-        above_zero_temp += 1
-    print(temperature)
+for delta in delta_ts:
+    temperature = 0.0 # starting temperature at 0
+    above_zero_count = 0.0 # track how many times temp is above 0
+    num_steps = int(1 / delta) # change delta_ts index for different values 
+    for _ in range(num_steps): 
+        Z = np.random.standard_normal()  
+        temperature_change = sigma*sqrt(delta)*Z  # this is based off a stochastic process. change delta_ts[x] for different time increments
+        temperature += temperature_change 
+        if temperature > 0.0:
+            above_zero_count += 1.0
+    p = above_zero_count/float(num_steps) 
+    print(f"P for delta_ts = {delta} is: {p}") 
 
-print ("P is: " + str(above_zero_temp/num_simulations))
