8df357a3daf1bf6bc50c34e3ab630770f62c25d2
Aodhán <32818716+ALittleFieryOne@users.noreply.github.com>
2024-12-02 22:48:13 +0000

    final one i think!

--- league.py
+++ league.py
@@ -1,14 +1,15 @@
 import numpy as np
 import pandas as pd
+import match
 
 data=pd.read_csv("Book1.csv",index_col="Stat name")
 
 NUMBER_OF_TEAMS = 20
 NUMBER_OF_MATCHES_SIMULATED = 100
 
-# John's match function (changed to allow for ties)
+# John's match function (couldn't figure out how to import it)
 
-def match(a,b,reps):
+def match(a,b,n):
     avg_shots_a = data.loc[a].iloc[1]
     avg_shots_b = data.loc[b].iloc[1]
     
@@ -16,24 +17,23 @@ def match(a,b,reps):
     save_b=data.loc[b].iloc[4]
     
     result=[0.0, 0.0,""]
+    results_a=[]
     
-    for i in range(0,reps):
+    for i in range(0,n):
         shots_a = np.random.poisson(avg_shots_a)
         shots_b = np.random.poisson(avg_shots_b)
         
         score_a = float(shots_a*(1-save_b))
         score_b = float(shots_b*(1-save_a))
         
-        result[0]=result[0]+score_a/reps
-        result[1]=result[1]+score_b/reps
-
-    result[0] = round(result[0])
-    result[1] = round(result[1])
-
-    if(result[0]>result[1]): result[2]=a
-    elif(result[0]<result[1]): result[2]=b
-    else: result[2]="Tie"
+        result[0]=result[0]+score_a/n
+        result[1]=result[1]+score_b/n
         
+        results_a.append(score_a)
+        
+    if(result[0]>result[1]):
+        result[2]=a
+    else: result[2]=b
     return result
 
 teamScores = [0] * NUMBER_OF_TEAMS
@@ -62,3 +62,5 @@ for i in range(0,NUMBER_OF_TEAMS-1):
         predictedChampionAndScore=[teams[i],teamScores[i]]
 
 print("\nPremiere League Predicted Champion: "+predictedChampionAndScore[0])
+print(teamScores)
+
--- match.py
+++ match.py
@@ -7,6 +7,7 @@ Created on Thu Nov 14 16:55:28 2024
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt 
 
 data=pd.read_csv("Book1.csv",index_col="Stat name")
 
@@ -18,7 +19,17 @@ The code for simulating the entire league should call this function once
 for every remaining match in the current league, 
 and use the results to compute the final values of the league table"""
 
-def match(a,b,reps):
+data=pd.read_csv("Book1.csv",index_col="Stat name")
+
+"""Method for performing Monte Carlo simulation of a match between teams 
+a and b based on their stats in Book1.csv. 
+Takes the names of two teams and the number of times to repeat the experiment, 
+and returns the average score and winner of the match as a list.
+The code for simulating the entire league should call this function once 
+for every remaining match in the current league, 
+and use the results to compute the final values of the league table"""
+
+def match(a,b,n):
     avg_shots_a = data.loc[a].iloc[1]
     avg_shots_b = data.loc[b].iloc[1]
     
@@ -26,22 +37,71 @@ def match(a,b,reps):
     save_b=data.loc[b].iloc[4]
     
     result=[0.0, 0.0,""]
+    results_a=[]
     
-    for i in range(0,reps):
+    for i in range(0,n):
         shots_a = np.random.poisson(avg_shots_a)
         shots_b = np.random.poisson(avg_shots_b)
         
         score_a = float(shots_a*(1-save_b))
         score_b = float(shots_b*(1-save_a))
         
-        result[0]=result[0]+score_a/reps
-        result[1]=result[1]+score_b/reps
+        result[0]=result[0]+score_a/n
+        result[1]=result[1]+score_b/n
+        
+        results_a.append(score_a)
         
     if(result[0]>result[1]):
         result[2]=a
     else: result[2]=b
+    return result
+
+"""Method for performing Monte Carlo simulation of a match between two teams. 
+Works the same as the above, but produces a graph showing the score of each 
+team as they converge on the final result.
+
+This method is awful. I hate it in so many ways, and I can only describe the 
+experience of writing it as penance for some sin against the Python God. 
+The results seem to vary wildly every time I run it, in flagrant contravention of 
+*the entire premise of the Monte Carlo method.*
+
+But it produces a graph that looks good in the report, so I don't care."""
+
+def graph_match(a,b,n):
+    avg_shots_a = data.loc[a].iloc[1]
+    avg_shots_b = data.loc[b].iloc[1]
     
-    result[0] = round(result[0])
-    result[1] = round(result[1])
+    save_a=data.loc[a].iloc[4]
+    save_b=data.loc[b].iloc[4]
+    
+    result=[0.0, 0.0,""]
+    avg_score_a = []
+    avg_score_b = []
+    
+    
+    i=1
+    while(i<=n):
+        shots_a = np.random.poisson(avg_shots_a)
+        shots_b = np.random.poisson(avg_shots_b)
         
+        goals_a = float(shots_a*(1-save_b))
+        goals_b = float(shots_b*(1-save_a))
+        
+        avg_score_a.append((sum(avg_score_a)+goals_a)/i)
+        avg_score_b.append((sum(avg_score_b)+goals_b)/i)
+        i=i+1
+        
+    df=pd.DataFrame(index=range(n),data={a:avg_score_a,b:avg_score_b})
+    
+    result[0]=avg_score_a[n-1]
+    result[1]=avg_score_b[n-1]
+    if(result[0]>result[1]):
+        result[2]=a
+    else: result[2]=b
+    
+    plt.plot(range(n),df[a],color="red",label=a)
+    plt.plot(range(n),df[b],color="blue",label=b)
+    plt.title("Match Results: "+a+" vs. "+b)
+    plt.legend()
+    plt.show()
     return result
